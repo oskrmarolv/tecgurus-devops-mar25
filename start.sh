6ccd1d782030ca481b85f2c071caac46c6539976
Guest <oscar.marq.olv@gmail.com>
2025-04-11 20:29:08 -0600

    [ CHANGE ] Docker Agregando script de construccion `build.sh`
     Changes to be committed:
    	new file:   build.sh
    	modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -
 
+#
+# Command help:
+#	...$./start.sh <project_absolut_path>
+#
 
+
+# var's
 PROJECT_PATH=$1;
 
 DOCKER_IMAGE="tecgurus/devops:ejemplo-001";
 DOCKER_CONTAINER_NAME="tecgurus_app_service";
 
+
+# command...
 docker run -d --name $DOCKER_CONTAINER_NAME \
   --env NODE_ENV='development'
   -v $PROJECT_PATH:/container/app \
